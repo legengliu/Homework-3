066c30669280f3e4edc0910635280b5c0933122e
legengliu <legengliu@berkeley.edu>
2017-10-14 21:22:39 -0700

    complete goodauction & badauction

--- contracts/BadAuction.sol
+++ contracts/BadAuction.sol
@@ -12,10 +12,27 @@ contract BadAuction is AuctionInterface {
 	 */
 	function bid() payable external returns (bool) {
 		// YOUR CODE HERE
+		if (msg.value <= highestBid) {
+			msg.sender.transfer(msg.value);
+			return false;
+		}
+
+		if (highestBidder != 0) {
+			if (!highestBidder.send(highestBid)) {
+				msg.sender.transfer(msg.value);
+				return false;
+			}
+		}
+
+		highestBid = msg.value;
+		highestBidder = msg.sender;
+		return true;
+
 	}
 
 	/* Give people their funds back */
 	function () payable {
 		// YOUR CODE HERE
+		msg.sender.transfer(msg.value);
 	}
 }
